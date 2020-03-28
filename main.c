226b4a931554768616fcf1d8fe5510e0b6c1bb1b
Dmitry <44711815+Varvors@users.noreply.github.com>
2020-03-28 13:23:22 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int count_digits(char *itsString)
+{
+  int i, j, dig=0;
+
+  for (i=0; i<10; i++)
+  {
+  for (j = 48; j < 58; j++)
+    if (itsString[i]==j)
+      {dig++;}
+
+  }
+  
+return dig;
+}
+
+int main()
+{
+
+  char itsString[10];
+
+  printf( "Input string: " );
+
+  fgets( itsString, 10, stdin );
+  
+  printf("dig count:%d", count_digits(itsString));
+
+return 0;
+}
